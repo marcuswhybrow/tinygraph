a8f42cc23cb2498f415139c842e1d1d8acb5eff9
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-15 17:56:23 +0000

    Optimised the DB querying on the dashboard to ALWAYS only query 3 times.

--- tinygraph/apps/dashboard/views.py
+++ tinygraph/apps/dashboard/views.py
@@ -1,5 +1,5 @@
 from django.views.generic.simple import direct_to_template
-from dashboard.models import Board
+from dashboard.models import Board, Item, Connection
 
 def index(request):
     try:
@@ -7,6 +7,15 @@ def index(request):
     except Board.DoesNotExist:
         board = None
     
+    items = []
+    connections = []
+    
+    if board is not None:
+        items = Item.objects.filter(board=board)
+        connections = Connection.objects.select_related().filter(from_item__board=board)
+    
     return direct_to_template(request, 'dashboard/index.html', {
         'board': board,
+        'items': items,
+        'connections': connections,
     })
