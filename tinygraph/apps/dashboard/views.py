31f0341292a344d2a395087b98f800e38f650080
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-03-25 19:42:12 +0000

    Added a dashboard-edit-panel div which slides up from the bottom of the page when in edit mode.

--- tinygraph/apps/dashboard/views.py
+++ tinygraph/apps/dashboard/views.py
@@ -1,7 +1,9 @@
 from django.views.generic.simple import direct_to_template
 from dashboard.models import Board, Item, Connection
+from devices.models import Device
 
 def index(request):
+    # For now assum the first Board in the database is the one to display
     try:
         board = Board.objects.get(pk=1)
     except Board.DoesNotExist:
@@ -10,12 +12,19 @@ def index(request):
     items = []
     connections = []
     
+    # If the board this page is displaying exists, then get its items and
+    # conntections. It is far more efficient to do the queries here, than
+    # in the template later on.
     if board is not None:
         items = Item.objects.filter(board=board)
         connections = Connection.objects.select_related().filter(from_item__board=board)
+    
+    # All of the devices in the system
+    devices = Device.objects.select_related()
     
     return direct_to_template(request, 'dashboard/index.html', {
         'board': board,
         'items': items,
         'connections': connections,
+        'devices': devices,
     })
