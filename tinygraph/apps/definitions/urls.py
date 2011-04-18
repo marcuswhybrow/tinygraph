7d223ef63f3a29e2b25762c1e72a59508429aac8
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-04-18 17:35:16 +0100

    Added basic delete page for packages.

--- tinygraph/apps/definitions/urls.py
+++ tinygraph/apps/definitions/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from tinygraph.apps.definitions.models import Package
 
 urlpatterns = patterns('definitions.views',
@@ -8,6 +8,11 @@ urlpatterns = patterns('definitions.views',
     url(r'^packages/$', 'package_list', name='package_list'),
     url(r'^packages/add/$', CreateView.as_view(model=Package, template_name='definitions/packages/package_form.html'), name='package_add'),
     url(r'^packages/(?P<package_slug>[-\w]+)/$', 'package_detail', name='package_detail'),
+    url(r'^packages/(?P<slug>[-\w]+)/delete$', DeleteView.as_view(
+        model=Package,
+        template_name='definitions/packages/package_delete.html',
+        success_url='/data/packages/',
+    ), name='package_delete'),
 
     url(r'^$', 'data_object_list', name='data_object_list'),
 )
