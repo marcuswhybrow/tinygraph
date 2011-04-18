2d62d7d447cc5742a90d2f2ae01d43f7de7b6eb1
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-04-18 17:19:12 +0100

    Added the most basic "add new package" page.

--- tinygraph/apps/definitions/urls.py
+++ tinygraph/apps/definitions/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls.defaults import *
+from django.views.generic.edit import CreateView
+from tinygraph.apps.definitions.models import Package
 
 urlpatterns = patterns('definitions.views',
     url(r'^mibs/$', 'mib_upload_list', name='mib_upload_list'),
     
     url(r'^packages/$', 'package_list', name='package_list'),
+    url(r'^packages/add/$', CreateView.as_view(model=Package, template_name='definitions/packages/package_form.html'), name='package_add'),
     url(r'^packages/(?P<package_slug>[-\w]+)/$', 'package_detail', name='package_detail'),
 
     url(r'^$', 'data_object_list', name='data_object_list'),
