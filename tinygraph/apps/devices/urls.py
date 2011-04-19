bed47b083d0c80d1d281c72bb7870596d65cad1a
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-04-19 19:10:41 +0100

    Added a device package page which displays a list of data the package has collected for the device in question.

--- tinygraph/apps/devices/urls.py
+++ tinygraph/apps/devices/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from tinygraph.apps.rules.views import package_instance_detail
 
 urlpatterns = patterns('devices.views',
     url(r'^add/$', 'device_add', name='device_add'),
@@ -7,6 +8,7 @@ urlpatterns = patterns('devices.views',
     url(r'^d/(?P<device_slug>[-\w]+)/data-objects/$', 'device_data_object_list', name='device_data_object_list'),
     url(r'^d/(?P<device_slug>[-\w]+)/delete/$', 'device_delete', name='device_delete'),
     url(r'^d/(?P<device_slug>[-\w]+)/edit/$', 'device_edit', name='device_edit'),
+    url(r'^d/(?P<device_slug>[-\w]+)/(?P<package_slug>[-\w]+)/$', package_instance_detail, name='package_instance_detail'),
     
     url(r'^$', 'device_list', name='device_list'),
 )
--- tinygraph/apps/rules/views.py
+++ tinygraph/apps/rules/views.py
@@ -1,1 +1,59 @@
-# No views
+from django.shortcuts import get_object_or_404
+from django.views.generic.simple import direct_to_template
+from tinygraph.apps.devices.models import Device
+from tinygraph.apps.definitions.models import Package
+from tinygraph.apps.rules.models import PackageInstance
+from tinygraph.apps.data.models import DataInstance
+from django.db.models import Count
+import datetime
+import itertools
+
+def keyfunc(data_instance):
+    return '%s.%s' % (data_instance.data_object, data_instance.suffix)
+
+SINGULAR_VALUE_TYPES = ['octet_string', 'object_identifier', 'gauge', 'integer', 'time_ticks']
+
+def package_instance_detail(request, device_slug, package_slug):
+    device = get_object_or_404(Device, slug=device_slug)
+    package = get_object_or_404(Package, slug=package_slug)
+    package_instance = get_object_or_404(PackageInstance, device=device, package=package)
+    rules = package_instance.rules.select_related()
+    
+    limit = datetime.datetime.now() - datetime.timedelta(days=1)
+    
+    rule_pairs = []
+    for rule in rules:
+        data_instances = rule.instances.filter(created__gte=limit).select_related()
+        
+        data_objects = []
+        data = sorted(data_instances, key=keyfunc)
+        for key, group in itertools.groupby(data, key=keyfunc):
+            group = list(group)
+            
+            data = None
+            data_instance = group[-1]
+            data_object = data_instance.data_object
+            suffix = data_instance.suffix
+            value_type = data_instance.value_type
+            
+            if value_type in SINGULAR_VALUE_TYPES:
+                data = data_instance.value
+            else:
+                data = group
+            
+            data_objects.append(({
+                'data_object': data_object,
+                'suffix': suffix,
+                'value_type': value_type,
+            }, data))
+        
+        rule_pairs.append((rule, data_objects))
+        
+    
+    return direct_to_template(request, 'rules/package_instance_detail.html', {
+        'device': device,
+        'package': package,
+        'package_instance': package_instance,
+        'rules': rule_pairs,
+        'singular_value_types': SINGULAR_VALUE_TYPES,
+    })
