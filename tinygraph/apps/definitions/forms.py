73e6cf1db608a0aeb6a7b36f8c0a33900099695b
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-04-22 20:38:22 +0100

    New Package page now works.

--- tinygraph/apps/definitions/forms.py
+++ tinygraph/apps/definitions/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from tinygraph.apps.definitions.models import MibUpload
+from tinygraph.apps.definitions.models import MibUpload, Package
 
 class MibUploadForm(forms.ModelForm):
     class Meta:
         model = MibUpload
         exclude = ('system', 'already_existed')
+
+class PackageForm(forms.ModelForm):
+    class Meta:
+        model = Package
+        exclude = ('data_objects', 'editable')
--- tinygraph/apps/definitions/urls.py
+++ tinygraph/apps/definitions/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls.defaults import *
 from django.views.generic.edit import CreateView, DeleteView
 from tinygraph.apps.definitions.models import Package
+from tinygraph.apps.definitions.forms import PackageForm
 
 urlpatterns = patterns('definitions.views',
     url(r'^mibs/$', 'mib_upload_list', name='mib_upload_list'),
     
     url(r'^packages/$', 'package_list', name='package_list'),
-    url(r'^packages/add/$', CreateView.as_view(model=Package, template_name='definitions/packages/package_form.html'), name='package_add'),
+    url(r'^packages/add/$', CreateView.as_view(
+        model=Package,
+        template_name='definitions/packages/package_form.html',
+        form_class=PackageForm,
+    ), name='package_add'),
     url(r'^packages/(?P<package_slug>[-\w]+)/$', 'package_detail', name='package_detail'),
     url(r'^packages/(?P<slug>[-\w]+)/delete$', DeleteView.as_view(
         model=Package,
