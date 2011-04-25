7295a5c7718d6ede20c71def419503166ed7b75b
Marcus Whybrow <marcus@marcuswhybrow.net>
2011-04-25 16:00:32 +0100

    Removed the /d/ from the /devices/d/vhost/intefaces/ type path because it was getting on my nerves!

--- tinygraph/apps/devices/urls.py
+++ tinygraph/apps/devices/urls.py
@@ -4,11 +4,11 @@ from tinygraph.apps.rules.views import package_instance_detail
 urlpatterns = patterns('devices.views',
     url(r'^add/$', 'device_add', name='device_add'),
     url(r'^test/$', 'test', name='test'),
-    url(r'^d/(?P<device_slug>[-\w]+)/$', 'device_detail', name='device_detail'),
-    url(r'^d/(?P<device_slug>[-\w]+)/data-objects/$', 'device_data_object_list', name='device_data_object_list'),
-    url(r'^d/(?P<device_slug>[-\w]+)/delete/$', 'device_delete', name='device_delete'),
-    url(r'^d/(?P<device_slug>[-\w]+)/edit/$', 'device_edit', name='device_edit'),
-    url(r'^d/(?P<device_slug>[-\w]+)/(?P<package_slug>[-\w]+)/$', package_instance_detail, name='package_instance_detail'),
+    url(r'^(?P<device_slug>[-\w]+)/$', 'device_detail', name='device_detail'),
+    url(r'^(?P<device_slug>[-\w]+)/data-objects/$', 'device_data_object_list', name='device_data_object_list'),
+    url(r'^(?P<device_slug>[-\w]+)/delete/$', 'device_delete', name='device_delete'),
+    url(r'^(?P<device_slug>[-\w]+)/edit/$', 'device_edit', name='device_edit'),
+    url(r'^(?P<device_slug>[-\w]+)/(?P<package_slug>[-\w]+)/$', package_instance_detail, name='package_instance_detail'),
     
     url(r'^$', 'device_list', name='device_list'),
 )
